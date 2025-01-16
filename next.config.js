3a1b7b8dd07d6116f87f104a1c6436bc5310160b
NikitaHub-gru <144660807+NikitaHub-gru@users.noreply.github.com>
2025-01-16 22:51:21 +0700

    dell /out

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	output: 'export',
+  // output: "export",
   swcMinify: false,
   typescript: {
     ignoreBuildErrors: true,
@@ -8,6 +8,6 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
